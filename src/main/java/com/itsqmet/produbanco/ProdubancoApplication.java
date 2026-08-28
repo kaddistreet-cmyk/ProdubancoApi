62130cff45e0ebc6653e3c257239a92210564141
kaddistreet-cmyk <kaddistreet@gmail.com>
2026-08-27 21:59:25 -0500

    chore(proyecto): inicializa la API de Produbanco

---
+++ src/main/java/com/itsqmet/produbanco/ProdubancoApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.produbanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdubancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdubancoApplication.class, args);
+    }
+
+}
